dc9cd6da7ea8d83e78984289c1c5518ae2ca6c20
Maxine Lui <maxinel@andrew.cmu.edu>
2022-12-13 17:37:36 -0500

    Recent version

---
+++ website/README.md
@@ -0,0 +1,10 @@
+Welcome to Easymoji!
+
+How to use
+1. Download this github
+2. Use your terminal to travel to the folder Easymoji/website/resources
+3. Write "node app.js" to start the local server
+4. In your browser, look up "localhost:3000"
+5. Write easymoji code in the editor (see the writeup here)
+6. Press compile
+7. Copy the output!
--- website/resources/app.js
+++ website/resources/app.js
@@ -6,7 +6,6 @@ const { exec } = require("child_process");
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
-
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
@@ -19,14 +18,10 @@ router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'))
 });
 
-app.post('/idk', function(req, res) {
+app.post('/codegen', function(req, res) {
     var user_input = req.body.user_input;
-    // console.log(user_input)
 
-    fs.writeFile("write_file.txt", user_input, err => {
-    // if (err) console.log(`Failed to write file: ${err}`);
-    // else console.log("File written.");
-    });
+    fs.writeFile("write_file.txt", user_input, err => {});
 
     exec("cp ./write_file.txt ../../codegen/user_input.txt; cd ..; cd ..; cd codegen; dune exec ./test.exe user_input.txt", (error, stdout, stderr) => {
         if (error) {
@@ -37,35 +32,7 @@ app.post('/idk', function(req, res) {
             console.log(`stderr: ${stderr}`);
             return;
         }
-        // console.log(`stdout: ${stdout}`);
         res.write(JSON.stringify(stdout))
         res.end()
     });
 });
-
-// app.post('/test', urlencodedParser, function(req, res) {
-//     var user_input = req.body.user_input;
-//     console.log(user_input)
-
-//     fs.writeFile("write_file.txt", user_input, err => {
-//     if (err) console.log(`Failed to write file: ${err}`);
-//     else console.log("File written.");
-//     });
-
-//     exec("cp ./write_file.txt ../../codegen/user_input.txt; cd ..; cd ..; cd codegen; dune exec ./test.exe user_input.txt", (error, stdout, stderr) => {
-//         if (error) {
-//             console.log(`error: ${error.message}`);
-//             return;
-//         }
-//         if (stderr) {
-//             console.log(`stderr: ${stderr}`);
-//             return;
-//         }
-//         console.log(`stdout: ${stdout}`);
-//         res.write(stdout)
-//         res.end()
-//     });
-// });
-
-// console.log("Listening at " + serverUrl + ":" + port);
-// server.listen(port, serverUrl);
