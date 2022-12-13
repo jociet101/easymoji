03f8eb6e141d3474b2bc5c5b58ac1470469fc915
Maxine Lui <maxinel@andrew.cmu.edu>
2022-12-13 16:59:25 -0500

    it works

--- website/resources/app.js
+++ website/resources/app.js
@@ -1,26 +1,31 @@
-const bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: true });
-
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
 const { exec } = require("child_process");
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+console.log('Running at Port 3000');
 
 router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'))
 });
 
-app.post('/test', urlencodedParser, function(req, res) {
+app.post('/idk', function(req, res) {
     var user_input = req.body.user_input;
-    console.log(user_input)
+    // console.log(user_input)
 
     fs.writeFile("write_file.txt", user_input, err => {
-    if (err) console.log(`Failed to write file: ${err}`);
-    else console.log("File written.");
+    // if (err) console.log(`Failed to write file: ${err}`);
+    // else console.log("File written.");
     });
 
     exec("cp ./write_file.txt ../../codegen/user_input.txt; cd ..; cd ..; cd codegen; dune exec ./test.exe user_input.txt", (error, stdout, stderr) => {
@@ -32,18 +37,35 @@ app.post('/test', urlencodedParser, function(req, res) {
             console.log(`stderr: ${stderr}`);
             return;
         }
-        console.log(`stdout: ${stdout}`);
-        res.write(stdout)
+        // console.log(`stdout: ${stdout}`);
+        res.write(JSON.stringify(stdout))
         res.end()
     });
 });
-  
-//add the router
-app.use(express.static('./public'));
-app.use('/', router);
-app.listen(process.env.port || 3000);
 
-console.log('Running at Port 3000');
+// app.post('/test', urlencodedParser, function(req, res) {
+//     var user_input = req.body.user_input;
+//     console.log(user_input)
+
+//     fs.writeFile("write_file.txt", user_input, err => {
+//     if (err) console.log(`Failed to write file: ${err}`);
+//     else console.log("File written.");
+//     });
+
+//     exec("cp ./write_file.txt ../../codegen/user_input.txt; cd ..; cd ..; cd codegen; dune exec ./test.exe user_input.txt", (error, stdout, stderr) => {
+//         if (error) {
+//             console.log(`error: ${error.message}`);
+//             return;
+//         }
+//         if (stderr) {
+//             console.log(`stderr: ${stderr}`);
+//             return;
+//         }
+//         console.log(`stdout: ${stdout}`);
+//         res.write(stdout)
+//         res.end()
+//     });
+// });
 
 // console.log("Listening at " + serverUrl + ":" + port);
 // server.listen(port, serverUrl);
